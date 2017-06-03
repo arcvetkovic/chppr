546880cfe4c814d1cbac69a94c80437442c83907
Aleksandar Cvetkovic <arcvetkovic@protonmail.com>
2017-06-03 22:49:48 +0100

    Generate OAuth parameters

--- R/oauth-parameters.R
+++ R/oauth-parameters.R
@@ -31,6 +31,31 @@ oauth_url <- function() {
   )
 }
 
+oauth_parameters <- function(url, consumer, token = NULL, verifier = NULL) {
+  url <- split_url(url)
+  parameters <- list(oauth_callback = consumer$callback,
+                     oauth_consumer_key = consumer$consumer_key,
+                     oauth_nonce = paste(sample(c(letters, LETTERS, 0:9),
+                                                10, TRUE),
+                                         collapse = ""),
+                     oauth_signature_method = "HMAC-SHA1",
+                     oauth_token = token$token,
+                     oauth_timestamp = as.integer(Sys.time()),
+                     oauth_verifier = verifier,
+                     oauth_version = "1.0")
+  parameters <- parameters[sapply(parameters, function(x) !is.null(x))]
+  string <- c(url$query, parameters)
+  string <- stats::setNames(oauth_encode(string), oauth_encode(names(string)))
+  string <- string[order(names(string))]
+  string <- paste0(names(string), "=", string, collapse = "&")
+  key <- paste0(oauth_encode(consumer$secret), "&",
+                oauth_encode(token$secret))
+  string <- paste0("GET", "&", oauth_encode(url$base), "&",
+                   oauth_encode(string))
+  parameters$oauth_signature <- httr::hmac_sha1(key, string)
+  parameters[order(names(parameters))]
+}
+
 split_url <- function(url) {
   if (length(grep("[?]", url)) > 0) {
     base <- gsub("(.*)[?](.*)", "\\1", url)
