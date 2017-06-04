3a61d57f987884c04739be2f68e95787477dce04
Aleksandar Cvetkovic <arcvetkovic@protonmail.com>
2017-06-04 20:06:59 +0100

    Get an access token

--- R/oauth-flow.R
+++ R/oauth-flow.R
@@ -64,3 +64,49 @@ authorization_url <- function(consumer, request_token) {
   if (length(scope)) url <- paste0(url, "&scope=", paste(scope, collapse = ","))
   url
 }
+
+#' Access Token
+#'
+#' Get an access token.
+#'
+#' @param consumer A consumer, created by \code{\link{consumer}}.
+#' @param request_token A request token, created by \code{\link{request_token}}.
+#' @param verifier A verification code obtained after successful authorization.
+#'
+#' @return An access token, an object of class \code{"token"}.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Consumer key and secret below are for illustrative purposes only
+#' cns <- consumer("3qxJ4m36ONdmkQxVES9xW5",
+#'                 "nOhqxax1vDLElZPXgSzYOWIGWbDTQD1BpegtGPz4sCu")
+#' rtkn <- request_token(cns)
+#' aurl <- authorization_url(cns, rtkn)
+#' # Open in browser and enter your login name and password
+#' # to obtain your verification code
+#' browseURL(aurl)
+#' # Verification code below is for illustrative purposes only
+#' atkn <- access_token(cns, rtkn, "CzYLOvq9NniD5L8T")
+#' }
+access_token <- function(consumer, request_token, verifier) {
+  url <- oauth_url()$access_token
+  response <- httr::GET(url,
+                        oauth_header(url, consumer, request_token, verifier))
+  content <- httr::content(response, "text", "text/html", "UTF-8")
+  if (response$status_code != 200L) {
+    html <- xml2::read_html(content, "UTF-8")
+    title <- xml2::xml_find_first(html, "head/title")
+    title <- xml2::xml_text(title)
+    stop(title)
+  }
+  content <- strsplit(content, "&")[[1]]
+  content_fields <- sapply(strsplit(content, "="), `[`, 1)
+  content_values <- sapply(strsplit(content, "="), `[`, 2)
+  content <- as.list(stats::setNames(content_values, content_fields))
+  if (is.null(content$oauth_token))
+    stop("Cannot parse access token.")
+  if (is.null(content$oauth_token_secret))
+    stop("Cannot parse access token secret.")
+  token(content$oauth_token, content$oauth_token_secret)
+}
