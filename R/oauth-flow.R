865a4cf897893a5eb74a57462bcbe7f5aea84208
Aleksandar Cvetkovic <arcvetkovic@protonmail.com>
2017-06-04 20:19:30 +0100

    Get protected resources

--- R/oauth-flow.R
+++ R/oauth-flow.R
@@ -110,3 +110,45 @@ access_token <- function(consumer, request_token, verifier) {
     stop("Cannot parse access token secret.")
   token(content$oauth_token, content$oauth_token_secret)
 }
+
+#' Protected Resources
+#'
+#' Get protected resources (CHPP files in XML format).
+#'
+#' @param query A query string, e.g. \code{"file=teamdetails&version=3.4"}.
+#' @param consumer A consumer, created by \code{\link{consumer}}.
+#' @param access_token An access token, created by \code{\link{access_token}}.
+#' @param parse Parse the result using \code{\link[xml2]{read_xml}}?
+#'
+#' @return If \code{parse} is \code{FALSE}, a character vector, otherwise an
+#' object of class \code{"xml_document"}.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # Consumer key and secret below are for illustrative purposes only
+#' cns <- consumer("3qxJ4m36ONdmkQxVES9xW5",
+#'                 "nOhqxax1vDLElZPXgSzYOWIGWbDTQD1BpegtGPz4sCu")
+#' rtkn <- request_token(cns)
+#' aurl <- authorization_url(cns, rtkn)
+#' # Open in browser and enter your login name and password
+#' # to obtain your verification code
+#' browseURL(aurl)
+#' # Verification code below is for illustrative purposes only
+#' atkn <- access_token(cns, rtkn, "CzYLOvq9NniD5L8T")
+#' pres <- protected_resources("file=teamdetails&version=3.4", cns, atkn)
+#' }
+protected_resources <- function(query, consumer, access_token, parse = FALSE) {
+  if (substr(query, 1, 1) != "?") query <- paste0("?", query)
+  url <- paste0(oauth_url()$protected_resources, query)
+  response <- httr::GET(url, oauth_header(url, consumer, access_token))
+  content <- httr::content(response, "text", "text/html", "UTF-8")
+  if (response$status_code != 200L) {
+    html <- xml2::read_html(content, "UTF-8")
+    title <- xml2::xml_find_first(html, "head/title")
+    title <- xml2::xml_text(title)
+    stop(title)
+  }
+  if (parse) content <- xml2::read_xml(content)
+  content
+}
