e6a27b7e9e9738558fb731427d12383f025e05eb
Aleksandar Cvetkovic <arcvetkovic@protonmail.com>
2017-06-05 18:45:47 +0100

    Test request token

--- tests/testthat/test-oauth.R
+++ tests/testthat/test-oauth.R
@@ -7,3 +7,13 @@ test_that("create consumer and token", {
   tkn <- token("OpDVf2v0vuD31yW7", "ASyuUmShpiodN2WX")
   expect_is(tkn, "token")
 })
+
+test_that("get request token", {
+  cns <- consumer(Sys.getenv("CHPPR_CONSUMER_KEY"),
+                  Sys.getenv("CHPPR_CONSUMER_SECRET"),
+                  NULL)
+  expect_error(request_token(cns))
+  cns <- consumer(Sys.getenv("CHPPR_CONSUMER_KEY"),
+                  Sys.getenv("CHPPR_CONSUMER_SECRET"))
+  expect_is(request_token(cns), "token")
+})
