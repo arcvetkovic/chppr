812d93af9ec6c68eaad2b33392aa96b7b22e3fff
Aleksandar Cvetkovic <arcvetkovic@protonmail.com>
2017-06-07 19:31:53 +0100

    Test access token

--- tests/testthat/test-oauth.R
+++ tests/testthat/test-oauth.R
@@ -17,3 +17,19 @@ test_that("get request token", {
                   Sys.getenv("CHPPR_CONSUMER_SECRET"))
   expect_is(request_token(cns), "token")
 })
+
+test_that("get access token", {
+  cns <- consumer(Sys.getenv("CHPPR_CONSUMER_KEY"),
+                  Sys.getenv("CHPPR_CONSUMER_SECRET"))
+  rtkn <- request_token(cns)
+  aurl <- authorization_url(cns, rtkn)
+  session <- rvest::html_session(aurl)
+  form <- rvest::html_form(session)[[1]]
+  form <- rvest::set_values(form,
+                            txtUsername = Sys.getenv("CHPPR_USERNAME"),
+                            txtPassword = Sys.getenv("CHPPR_PASSWORD"))
+  session <- rvest::submit_form(session, form)
+  content <- httr::content(session$response)
+  vrf <- rvest::html_text(rvest::xml_nodes(content, "pre"))
+  expect_is(access_token(cns, rtkn, vrf), "token")
+})
