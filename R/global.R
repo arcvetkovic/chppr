65327e83c09cabf05e5fae5483ed2489eb00b16f
Aleksandar Cvetkovic <arcvetkovic@protonmail.com>
2017-06-10 21:54:27 +0100

    Download languages

---
+++ R/global.R
@@ -0,0 +1,28 @@
+#' Languages
+#'
+#' Download a data frame with available languages.
+#'
+#' @param consumer A consumer, created by \code{\link{consumer}}.
+#' @param access_token An access token, created by \code{\link{access_token}}.
+#'
+#' @return A data frame with two variables:
+#' \item{\code{language_id}}{Language ID.}
+#' \item{\code{language_name}}{Language name.}
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' df <- languages(consumer, access_token)
+#' }
+languages <- function(consumer, access_token) {
+  query <- "file=worldlanguages&version=1.2"
+  xml <- protected_resources(query, consumer, access_token, TRUE)
+  nodes <- xml2::xml_find_all(xml, "LanguageList/Language")
+  language_id <- xml2::xml_find_first(nodes, "LanguageID")
+  language_id <- xml2::xml_integer(language_id)
+  language_name <- xml2::xml_find_all(nodes, "LanguageName")
+  language_name <- xml2::xml_text(language_name)
+  data.frame(language_id,
+             language_name,
+             stringsAsFactors = FALSE)
+}
